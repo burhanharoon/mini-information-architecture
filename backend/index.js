509e9e3af93bb52a0eb508fd235dfc9631e2c4b4
burhan <2019cs626@student.uet.edu.pk>
2022-04-07 10:10:02 -0700

    Initial Commit

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express, { json } from 'express'
+import dotenv from "dotenv"
+// import connectDB from './config/db.js'
+import bodyparser from 'body-parser'
+
+dotenv.config();
+
+// connectDB();
+
+const app = express()
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send("Api is running dude damn")
+})
+
+// app.use('/api/products', productRoutes)
+// app.use('/api/users', userRoutes)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(5000, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))

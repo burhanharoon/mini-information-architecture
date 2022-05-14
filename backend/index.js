587a1cad893294b6f92116a5d0036d553c4d6d2e
Burhan Haroon <89704304+ez-sherlock@users.noreply.github.com>
2022-05-14 23:56:50 +0500

    Add port number variable

--- backend/index.js
+++ backend/index.js
@@ -26,4 +26,4 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(5000, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
